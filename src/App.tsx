21e708d13b1d9a510f85a6f47c628da9351a58f7
John Murphy <johnmurphy@Johns-MacBook-Pro.local>
2023-07-02 13:14:44 -0400

    just labelled papers now

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ function App() {
       >
         <AboutMe />
       </div>
-      <h1 className="projects-label">Publications/Papers</h1>
+      <h1 className="projects-label">Papers</h1>
       <a
         href="https://www.frontiersin.org/articles/10.3389/frvir.2022.968312/full"
         className="url-link"
